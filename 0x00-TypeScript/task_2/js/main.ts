9f3c9484b1db1b80ac959781db561a3b78221ad9
vanemcb <vanem2201@gmail.com>
2022-09-12 17:33:53 -0500

    Task 6

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -33,3 +33,17 @@ const createEmployee = (salary: number | string): Teacher | Director => {
 // console.log(createEmployee(200));
 // console.log(createEmployee(1000));
 // console.log(createEmployee('$500'));
+
+// Task 6
+function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+	return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface): String {
+	if (isDirector(employee)) {
+		return employee.workDirectorTasks();
+	}
+	return employee.workTeacherTasks();
+}
+// executeWork(createEmployee(200));
+// executeWork(createEmployee(1000));
