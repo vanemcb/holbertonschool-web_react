572c7fe78cb078bfb0c98f0b7c5c0853953a7604
vanemcb <vanem2201@gmail.com>
2022-09-12 17:02:22 -0500

    Task 2

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -8,11 +8,23 @@ interface Teacher {
 };
 
 const teacher3: Teacher = {
-	firstName: 'Vanessa',
-	fullTimeEmployee: true,
-	lastName: 'Castro',
-	location: 'Sabaneta',
+	firstName: 'John',
+	fullTimeEmployee: false,
+	lastName: 'Doe',
+	location: 'London',
 	contract: false,
 };
+// console.log(teacher3);
 
-console.log(teacher3);
+interface Directors extends Teacher {
+	numberOfReports: number,
+};
+
+const director1: Directors = {
+	firstName: 'John',
+	lastName: 'Doe',
+	location: 'London',
+	fullTimeEmployee: true,
+	numberOfReports: 17,
+};
+console.log(director1);
